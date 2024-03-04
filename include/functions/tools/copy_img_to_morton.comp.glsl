84ac730c3918fa1b6a8c6f2a39c2fb23275dbf33
Ludwig Leonard <lleonart1984@gmail.com>
2024-03-04 23:39:01 +0100

    some fixes in old referencing to rendervous

--- include/functions/tools/copy_img_to_morton.comp.glsl
+++ include/functions/tools/copy_img_to_morton.comp.glsl
@@ -9,15 +9,6 @@ layout(binding = 1, std430) uniform Custom {
     int output_dim;
 };
 
-int unpart1by1(int n){
-    n &= 0x55555555; // base10: 1431655765, binary: 1010101010101010101010101010101,  len: 31
-    n = (n ^ (n >> 1)) & 0x33333333; // base10: 858993459,  binary: 110011001100110011001100110011,   len: 30
-    n = (n ^ (n >> 2)) & 0x0f0f0f0f; // base10: 252645135,  binary: 1111000011110000111100001111,     len: 28
-    n = (n ^ (n >> 4)) & 0x00ff00ff; // base10: 16711935,   binary: 111111110000000011111111,         len: 24
-    n = (n ^ (n >> 8)) & 0x0000ffff; // base10: 65535,      binary: 1111111111111111,                 len: 16
-    return n;
-}
-
 void main(){
     uvec3 thread_id;
     if (!start_function(thread_id))
